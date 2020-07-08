4c2a7ed81efa68eabb907ec98f2fd2e74511f862
Hollis <54090150+colburnh@users.noreply.github.com>
2020-07-08 15:38:38 -0700

    Add files via upload

--- colburnh.adventure.c
+++ colburnh.adventure.c
@@ -1,19 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <math.h>
+
+char *findLatestDir() {
+    /* adapted from class material */
+    /* set up */
+    struct stat dirStat;
+    char directoryName[256];
+    char *latestDirName;
+    char *path = ".";
+
+    /* open directory */
+    DIR* currDir = opendir(path);
+    struct dirent *aDir;
+    time_t lastModifTime;
+    int i = 0;
+    char *prefix = "colburnh.rooms.";
+
+    while((aDir = readdir(currDir)) != NULL){
+        // Use strncmp to check if the directory name matches the prefix
+        if(strncmp(prefix, aDir->d_name, strlen(prefix)) == 0){
+            stat(aDir->d_name, &dirStat);
+            // Check to see if this is the directory with the latest modification time
+            if(i == 0 || difftime(dirStat.st_mtime, lastModifTime) > 0){
+                lastModifTime = dirStat.st_mtime;
+                memset(directoryName, '\0', sizeof(directoryName));
+                strcpy(directoryName, aDir->d_name);
+            }
+        i++;
+        }
+    }
+    /* create name of most recent directory */
+    latestDirName = malloc(sizeof(char) * (strlen(directoryName) + 1));
+    strcpy(latestDirName, directoryName);
+    return latestDirName;
+}
 
 int main(void) {
 
     /* find and open most recent set of rooms */
+    char *latestDirName = findLatestDir();
 
     char roomName[] = "test room";
-    char nextRoom[20];
+    //char nextRoom[20];
     int roomNum = 0;
+
+    char *buffer;
+    size_t bufsize = 32;
+    size_t characters; //wrong
+
+    buffer = (char *)malloc(bufsize * sizeof(char));
+    if (buffer == NULL) {
+        perror("Unable to allocate buffer\n");
+        exit(1);
+    }
 
     /* prompt user to choose a room */
     printf("CURRENT LOCATION: %s\n", roomName);
     printf("POSSIBLE CONNECTIONS: %s\n", roomName);
-    printf("Where to? ");
-    scanf("%s", nextRoom);
-    printf("We're going to %s!\n", nextRoom);  /* delete before submitting */
+    printf("Where to? >");
+    char nextRoom = getline(&buffer, &bufsize, stdin);
+    //scanf("%s", nextRoom);
+    printf("We're going to %s!\n", buffer);  /* delete before submitting */
 
     /* check validity */
 
--- colburnh.buildrooms.c
+++ colburnh.buildrooms.c
@@ -7,12 +7,23 @@
 #include <sys/stat.h>
 #include <time.h>
 #include <math.h>
+#include <stdbool.h>
 
 struct Room {
-    char *name;
+    char *name; // do these need to be pointers?
     char *type;
-    char *connection;
+    int dest;
     struct Room *next;
+};
+struct Room* next;
+
+struct roomList {
+    struct Room *head;
+};
+
+struct Graph {
+    int rooms;
+    struct roomList *connPtr;
 };
 
 void createDir(){
@@ -39,6 +50,8 @@ void createDir(){
 
     /* create directory with new directory name */
     mkdir(str, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    int closedir(DIR *dirp);
+    free(str);
 
     return;
 }
@@ -51,6 +64,7 @@ char *getFilePath(char *directoryName, char *fileName){
     strcpy(filePath, directoryName);
     strcat(filePath, "/");
     strcat(filePath, fileName);
+    free(filePath);
     return filePath;
 }
 
@@ -107,20 +121,37 @@ char *findLatestDir() {
     /* create name of most recent directory */
     latestDirName = malloc(sizeof(char) * (strlen(directoryName) + 1));
     strcpy(latestDirName, directoryName);
+    
     return latestDirName;
+}
+
+struct Graph* makeGraph(int numRooms) {
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->rooms = numRooms;
+
+    graph->connPtr = (struct roomList*)malloc(numRooms * sizeof(struct roomList));
+
+    int i;
+    for (i = 0; i < numRooms; i++) {
+        graph->connPtr[i].head = NULL;
+    }
+
+    return graph;
 }
 
 int makeRoom(int size, int usedNums[]){
     char names[10][9] = {"coffee", "tea", "water", "beer", "wine", "cocktail", "milk", "smoothie", "soda", "seltzer"};
     int roomNum;
-    /* check if room num has already been used */
+    char *roomPtr;
+
     for (int i = 0; i < 7; i++) {
 
-        /* create room file */
+        /* set up pointers */
         char *roomName = &names[i][0];
-        struct Room currentRoom = {.name = roomName};
-        printf("room name: %s\n", currentRoom.name);
+        struct Room *roomList[i], currentRoom = {.name = roomName};
+        roomList[i] = &currentRoom;
 
+        /* assign room type */
         if (size == 10){
             currentRoom.type = "START_ROOM";
         } else if (size == 9){
@@ -129,7 +160,6 @@ int makeRoom(int size, int usedNums[]){
             currentRoom.type = "MID_ROOM";
         }
 
-        printf("type: %s\n", currentRoom.type);
         size--;
 
         /* create room file */
@@ -147,7 +177,6 @@ int makeRoom(int size, int usedNums[]){
 
         /* get file path */
         char *filePath = getFilePath(latestDirName, fileName);
-        printf("filePath is %s\n", filePath);
         int fileDescriptor;
 
         /* create file */
@@ -159,8 +188,16 @@ int makeRoom(int size, int usedNums[]){
         }
 
         /* process file */
+        /* allocate space for room name line */
+        char roomNameLine[] = "ROOM NAME: ";
+        char *nameLine = calloc(strlen(roomName) + 9, sizeof(char));
 
+        /* concatenate roomNameLine and name of room */
+        strcpy(nameLine, roomNameLine);
+        strcat(nameLine, currentRoom.name);
         
+        /* write to file */
+        write(fileDescriptor, nameLine, strlen(nameLine) + 1);
 
         // Close the file descriptor
         close(fileDescriptor);
@@ -168,17 +205,163 @@ int makeRoom(int size, int usedNums[]){
     return size;
 }
 
+int getRandomRoom() {
+    return (rand() % 6);
+}
+
+struct Room* makeNewRoom(int dest) {
+    struct Room* newRoom = (struct Room*)malloc(sizeof(struct Room));
+    newRoom->dest = dest;
+    newRoom->next = NULL;
+
+    return newRoom;
+}
+
+// Returns true if Rooms x and y are the same Room, false otherwise
+bool isSameRoom(int roomX, int roomY) {
+    if (roomX == roomY)
+        return true;
+    else
+        return false;
+}
+
+// Connects Rooms x and y together, does not check if this connection is valid
+void connectRoom(struct Graph* graph, int roomX, int roomY) {
+    // Add an edge from src (X) to dest (Y).  A new node is added to the adjacency
+    // list of src.  The node is added at the begining
+    struct Room* newRoom = makeNewRoom(roomY);
+    newRoom->next = graph->connPtr[roomX].head;
+    graph->connPtr[roomX].head = newRoom;
+ 
+    // Since graph is undirected, add an edge from dest to src also
+    newRoom = makeNewRoom(roomX);
+    newRoom->next = graph->connPtr[roomY].head;
+    graph->connPtr[roomY].head = newRoom;
+    printf("success\n");
+}
+
+// Returns true if a connection from Room x to Room y already exists, false otherwise
+bool connectionAlreadyExists(struct Graph* graph, int roomX, int roomY) {
+    struct Room* pCrawl = graph->connPtr[roomX].head;
+    while (pCrawl) {
+        if (pCrawl->dest == roomY) {
+            return true;
+        }
+        pCrawl = pCrawl->next;
+    }
+    return false;
+}
+
+// Returns true if a connection can be added from Room x (< 6 outbound connections), false otherwise
+bool canAddConnectionFrom(struct Graph* graph, int roomX) {
+    int count = 0;
+    struct Room* pCrawl = graph->connPtr[roomX].head;
+    while (pCrawl) {
+        count++;
+        pCrawl = pCrawl->next;
+    }
+    if (count < 6) {
+        return true;
+    }
+    return false;
+}
+
+// Adds a random, valid outbound connection from a Room to another Room
+void addRandomConnection(struct Graph* graph) {
+    int roomA;
+    int roomB;
+
+    while(true) {
+        roomA = getRandomRoom();
+
+        if (canAddConnectionFrom(graph, roomA) == true)
+            break;
+    } 
+    do {
+        roomB = getRandomRoom();
+
+    } while(canAddConnectionFrom(graph, roomB) == false || isSameRoom(roomA, roomB) == true || connectionAlreadyExists(graph, roomA, roomB) == true); {
+        connectRoom(graph, roomA, roomB);  // TODO: Add this connection to the real variables, 
+        connectRoom(graph, roomB, roomA);  //  because this A and B will be destroyed when this function terminates
+    }
+}
+
+// Returns true if all rooms have 3 to 6 outbound connections, false otherwise
+bool isGraphFull(struct Graph* graph) {
+
+    int v;
+
+    /* traverse linked list to find number of elements in each list */  
+    for (v = 0; v < graph->rooms; ++v) {
+        int count = 0;
+        struct Room* pCrawl = graph->connPtr[v].head;
+        while (pCrawl) {
+            pCrawl = pCrawl->next;
+            count++;
+            if (pCrawl->next == NULL) {
+                if (count < 3) {
+                printf("not full\n");
+                return false;
+                }
+            }
+    /* if any list has fewer than 3 connections, the graph is not full */
+        }
+    }
+    return true;
+}
+
+int makeConnections(struct Graph* graph) {
+    printf("here?\n");
+    // Create all connections in graph
+    while (isGraphFull(graph) == false)
+    {
+        printf("not full yet\n");
+        addRandomConnection(graph);
+    }
+
+    return 0;
+}
+
+// Print the graph
+void printGraph(struct Graph* graph) {
+    int v;
+    for (v = 0; v < graph->rooms; ++v) {
+        struct Room* pCrawl = graph->connPtr[v].head;
+        printf("\n Adjacency list of vertex %d\n head ", v);
+        while (pCrawl) {
+            printf("-> %d", pCrawl->dest);
+            pCrawl = pCrawl->next;
+        }
+        printf("\n");
+    }
+}
+
 int main(void) {
+    
+    int numRooms = 7;
+
     /* create directory */
     createDir();
 
     /* choose 7 at random from 10 names */
     int size = 10;
-    
-    /* generate room files */
     int numList = getRoomNum();
-    makeRoom(size, &numList);
+
+
+    struct Graph *graph = makeGraph(numRooms);
+    // connectRoom(graph, 0, 1);
+    // connectRoom(graph, 0, 4);
+    // connectRoom(graph, 1, 2);
+    // connectRoom(graph, 1, 3);
+    // connectRoom(graph, 1, 4);
+    // connectRoom(graph, 2, 3);
+    // connectRoom(graph, 3, 4);
+    /* create room connections */
+    //makeConnections(graph);
+    printGraph(graph);
+
+    // /* generate room files */
+    // makeRoom(size, &numList);
     return 0;
 
-    /* create room connections */
 }
